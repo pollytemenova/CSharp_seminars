3de9274bf0c99caa33b9e7d0d855d7b94fb8a241
Polina <gorba4evap@mail.ru>
2023-06-16 22:51:56 +0300

    task 4 done

---
+++ Seminar_001-4/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(1,10);
+Console.WriteLine(number);    
+int count = 1;
+while (count<=number) 
+{
+int result = count % 2;
+
+if (result == 0)
+{
+Console.WriteLine(count);                    
+}  
+count++;                  
+}
