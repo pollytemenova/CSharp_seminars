5270df9227d269e2f534b8f385a61128b67769fb
Polina <gorba4evap@mail.ru>
2023-06-16 22:43:14 +0300

    task 3

---
+++ Seminar_001-3/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(1,10);
+Console.WriteLine(number);
+int result = number % 2;
+
+if (result == 0)
+{
+Console.WriteLine("число четное");
+}
+else
+{
+Console.WriteLine("число нечетное");                
+}
