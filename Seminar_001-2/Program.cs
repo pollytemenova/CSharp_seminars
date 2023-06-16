45ebc4fba39d40cf1dd9381aef44bd3565411bb4
Polina <gorba4evap@mail.ru>
2023-06-16 21:54:04 +0300

    task 2 done

---
+++ Seminar_001-2/Program.cs
@@ -0,0 +1,23 @@
+﻿int number1 = new Random().Next(1,10);
+int number2 = new Random().Next(1,10);
+int number3 = new Random().Next(1,10);
+int max = 0;
+Console.WriteLine(number1);
+Console.WriteLine(number2);
+Console.WriteLine(number3);
+if(number1>number2)
+{
+max = number1;
+}
+else
+{
+max = number2;
+}
+if (max>number3)
+{
+Console.WriteLine(max);
+}
+else 
+{
+Console.WriteLine(number3);
+}
