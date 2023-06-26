a5dffaf9740a974f2397feeefda51d575a0af8bb
Polina <gorba4evap@mail.ru>
2023-06-26 21:31:48 +0300

    Task 3 done

---
+++ Seminar_003-3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+int count = 1;
+while(count<=n)
+{
+                        Console.WriteLine($"{count} в кубе = {count*count*count}");
+                        count++;
+}
